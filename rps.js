4b6a2522d6b2db56fd7c896153f4d60d7b24f8b2
Izy266 <islamelkattawy@gmail.com>
2023-10-01 02:08:56 -0400

    Created buttons with winner at 5 points

--- rps.js
+++ rps.js
@@ -31,18 +31,47 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    let score = 0;
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Rock, Paper, or Scissors?")
-        let gameResult = playRound(playerSelection, getComputerChoice());
-        score += gameResult[1];
-        console.log(gameResult[0]);
-    }
-    if (score > 0) {
-        console.log(`You won with a score of ${score}`);
-    } else {
-        console.log(`You lost! with a score of ${score}`);
-    }
+    const rpsChoices = document.querySelectorAll(".rps-choice");
+    console.log(rpsChoices.length);
+    rpsChoices.forEach((choice) => {
+        choice.addEventListener('click', () => {
+
+            const playerChoice = choice.id;
+            let body = document.querySelector("body");
+            resultSection = document.querySelector("#result");
+            pscoreSection = document.querySelector("#player-score");
+            cscoreSection = document.querySelector("#comp-score");
+            const result = playRound(playerChoice, getComputerChoice());
+            playerScore = result[1] == 1 ? playerScore + 1 : playerScore;
+            compScore = result[1] == -1 ? compScore + 1 : compScore;
+            resultSection.textContent = result[0];
+            pscoreSection.textContent = `Your Score: ${playerScore}`;
+            cscoreSection.textContent = `Computer Score: ${compScore}`;
+            if ((playerScore == 5 || compScore == 5) && !won) {
+                won = true;
+                const winResult = document.createElement('div');
+                winResult.textContent = playerScore == 5
+                    ? "YOU WIN THIS TIME!!! Dare to try again!?"
+                    : "You will never beat me! Better luck next time.";
+
+                const tryAgain = document.createElement('button');
+                tryAgain.textContent = "Try Again";
+                tryAgain.addEventListener('click', () => {
+                    won = false;
+                    winResult.remove();
+                    tryAgain.remove();
+                    playerScore = 0;
+                    compScore = 0;
+                });
+
+                body.appendChild(winResult);
+                body.appendChild(tryAgain)
+            }
+        });
+    });
 }
 
+let playerScore = 0;
+let compScore = 0;
+let won = false;
 game();
